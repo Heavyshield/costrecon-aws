6397277ecab41237ea5282e47110e1dce5e93010
root <tlasserre.cloud@gmail.com>
2025-09-04 21:05:38 +0200

    refactor folder structure

---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+"""Utility modules for CostRecon."""
+
+from .pdf_report_generator import PDFReportGenerator
+from .cli_report_generator import print_console_report
+
+__all__ = ['PDFReportGenerator', 'print_console_report']
